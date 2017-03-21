2028d0e86ffc2e634f8089921c276c9b555dacd5
Prashant Khurana <prashantkhurana145@gmail.com>
2017-03-20 22:17:20 -0400

    test file

---
+++ CreativePropertyFilter2.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jmx.export.annotation.ManagedAttribute;
+import org.springframework.jmx.export.annotation.ManagedResource;
+
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+import com.test;
+
+import static java.util.Objects.requireNonNull;
