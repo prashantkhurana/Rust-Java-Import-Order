1cd1c5e894cb55b858b524e7ed8c75cec60f6f6a
Prashant Khurana <prashantkhurana145@gmail.com>
2017-03-22 11:57:41 -0400

    works. clean up

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use file::file_contents::LineOfCode;
 
 fn main() {
     match fix_import_order2() {
-        Ok(v) => print!("works!"),
+        Ok(_) => print!("works!"),
         Err(s) => print!("error {}", s)
     };
     //read_and_fix_temp2("/Users/PKhurana/opt/m6d/adserv-a1/adserv-serviceLayer/src/main/java/com/dstillery/duffman/converter/BidSummaryConverter.java");
@@ -19,14 +19,14 @@ fn main() {
 }
 
 fn fix_import_order2() -> Result<(i32), Error> {
-    let mut file_names:Vec<String> = Vec::new();
+    let file_names:Vec<String> = Vec::new();
     let file_names:Vec<String> = file::get_file_names_in_directory3("/Users/PKhurana/opt/m6d/adserv-a1", file_names).unwrap();
     println!("final paths of all files {:?}", file_names);
 
     for file_name in file_names {
-        read_and_fix_temp2(&file_name);
+        try!(read_and_fix_temp2(&file_name));
     }
-    Ok(3)
+    Ok(1)
 }
 
 
@@ -39,23 +39,20 @@ fn read_and_fix_temp2(file_name:&str) -> Result<(i32), Error> {
         if contents.ends_with("\n")  {
             ends_with_n = true;
         }
-        let mut file_contents:File = try!(File::open(file_name));
+        let file_contents:File = try!(File::open(file_name));
 
-        let mut file = BufReader::new(file_contents);
+        let file = BufReader::new(file_contents);
         let mut v:Vec<LineOfCode> = Vec::new();
         let mut b:Vec<String> = Vec::new();
         let mut a:Vec<String> = Vec::new();
 
         let mut file_contents2:File = try!(File::create("/Users/PKhurana/code/Rust-Java-Import-Order/test.java"));
 use std::io::Write;
-use std::io::Seek;
-use std::io::SeekFrom;
-        for (num, line) in file.lines().enumerate() {
-            let mut l:String = line.unwrap();
+        for line in file.lines() {
+            let l:String = line.unwrap();
             if l.starts_with("import") {
                 let c = LineOfCode { line:l };
                 v.push(c);
-                l = String::from("222");
             } else if v.len() == 0 {
                 b.push(l);
             } else if l.is_empty() && a.len() == 0 {
@@ -71,7 +68,7 @@ use std::io::SeekFrom;
 
 
         for (i,line2) in b.iter().enumerate() {
-            file_contents2.write_all(line2.as_bytes());
+            file_contents2.write_all(line2.as_bytes())?;
             if i == b.len() -1 {
                 print!("not writing");
             } else {
@@ -83,14 +80,12 @@ use std::io::SeekFrom;
         }
 
 
-
-
          for line2 in v {
-            let mut l:String = line2.line;
+            let l:String = line2.line;
             if l == "\n" {
                 //print!("new line found");
             }
-             file_contents2.write_all(l.as_bytes());
+             file_contents2.write_all(l.as_bytes())?;
              if l != "\n" {
              file_contents2.write(b"\n").unwrap();   
             }
@@ -99,7 +94,7 @@ use std::io::SeekFrom;
 
         println!("ends with {}",ends_with_n);
         for (i,line2) in a.iter().enumerate() {
-            file_contents2.write_all(line2.as_bytes());
+            file_contents2.write_all(line2.as_bytes())?;
             if i == a.len() -1 && ends_with_n == false {
                 print!("not writing");
             } else {
@@ -111,18 +106,18 @@ use std::io::SeekFrom;
         println!("file_contents {:?}", file_name);
 
         try!(fs::rename("/Users/PKhurana/code/Rust-Java-Import-Order/test.java", file_name));
-        Ok(2)
+        Ok(1)
 }
 
 fn add_new_line(v1 : Vec<LineOfCode>) -> Vec<LineOfCode>{
 
     let mut old :i32 = -1;
-    let mut new :i32 = -1;
+    let mut new :i32;
     let mut v:Vec<LineOfCode> = Vec::new();
 
 
     for line2 in v1 {
-            new = getI(&line2);
+            new = get_i(&line2);
             if old != new && old != -1 {
                let c: LineOfCode = LineOfCode { line:String::from("\n")};
                v.push(c);
@@ -133,7 +128,7 @@ fn add_new_line(v1 : Vec<LineOfCode>) -> Vec<LineOfCode>{
     v
 }
 
-fn getI(point : &LineOfCode) -> i32 {
+fn get_i(point : &LineOfCode) -> i32 {
     let mut selfi:i32 = 5;
     let mut i:i32 = 0;
 
